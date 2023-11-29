337b1f96fbaee7427347ccb758a8c4d2ced09036
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-29 19:27:05 +0300

    concurrency semaphore concept

---
+++ src/concurrency/semaphore/login/LoginClient.java
@@ -0,0 +1,19 @@
+package concurrency.semaphore.login;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class LoginClient {
+    public static void main(String[] args) {
+        int slots = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(slots);
+        LoginQueue loginQueue = new LoginQueue(slots);
+        IntStream.range(0, slots)
+                .forEach(user -> executorService.execute(loginQueue::tryLogin));
+        executorService.shutdown();
+
+        System.out.println(loginQueue.availableSlots());
+        System.out.println(loginQueue.tryLogin());
+    }
+}
