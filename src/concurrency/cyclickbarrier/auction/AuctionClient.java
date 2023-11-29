99ceab2635c97b32d4659ff458c81c1c175c2fa9
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-29 16:33:19 +0300

    concurrency cyclickbarrier concept

---
+++ src/concurrency/cyclickbarrier/auction/AuctionClient.java
@@ -0,0 +1,25 @@
+package concurrency.cyclickbarrier.auction;
+
+import java.util.Random;
+
+/**
+ * Процесс проведения аукциона подразумевает корректное использование класса CyclicBarrier.
+ * Класс Auction определяет список конкурирующих предложений от клиентов и размер барьера.
+ * Чтобы приложение работало корректно, необходимо, чтобы размер списка совпадал со значением константы BIDS_NUMBER.
+ * Барьер инициализируется потоком определения победителя торгов, который запустится после того, как все предложения
+ * будут объявлены. Если потоков будет запущено больше чем размер барьера, то «лишние» предложения могут быть не учтены
+ * при вычислении победителя, если же потоков будет меньше, то приложение окажется в состоянии deadlock.
+ * Для предотвращения подобных ситуаций следует использовать метод await() с параметрами.
+ */
+public class AuctionClient {
+
+    public static void main(String[] args) {
+        Auction auction = new Auction();
+        int startPrice = new Random().nextInt(100);
+        for (int i = 0; i < auction.BIDS_NUMBER; i++) {
+            Bid thread = new Bid(i, startPrice, auction.getBarrier());
+            auction.add(thread);
+            thread.start();
+        }
+    }
+}
